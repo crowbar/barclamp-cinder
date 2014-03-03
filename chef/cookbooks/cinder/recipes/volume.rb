61f31b0adacca663204b70b5a2c8be8e0a18a075
Oğuz Yarımtepe <oguzy@turksat.com.tr>
2014-03-03 12:16:06 +0200

    Combined two lined bash code to a single line

--- chef/cookbooks/cinder/recipes/volume.rb
+++ chef/cookbooks/cinder/recipes/volume.rb
@@ -81,8 +81,7 @@ def make_loopback_volume(node,volname)
   end
 
   bash "create volume group" do
-    code "losetup --show -f #{fname}"
-    code "vgcreate #{volname} `losetup -j #{fname} | cut -f1 -d:`"
+    code "losetup --show -f #{fname} && vgcreate #{volname} `losetup -j #{fname} | cut -f1 -d:`"
     not_if "vgs #{volname}"
   end
 end
