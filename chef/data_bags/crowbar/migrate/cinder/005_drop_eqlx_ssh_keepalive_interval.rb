3bf8250037c3ab321d988a2ddb5585f3f4ada4c8
Vincent Untz <vuntz@suse.com>
2015-09-09 11:11:51 +0200

    Add note that this got moved to merged repo

--- chef/data_bags/crowbar/migrate/cinder/005_drop_eqlx_ssh_keepalive_interval.rb
+++
@@ -1,9 +0,0 @@
-def upgrade ta, td, a, d
-  a["volume"]["eqlx"].delete('eqlx_ssh_keepalive_interval')
-  return a, d
-end
-
-def downgrade ta, td, a, d
-  a["volume"]["eqlx"]['eqlx_ssh_keepalive_interval'] = ta["volume"]["eqlx"]['eqlx_ssh_keepalive_interval']
-  return a, d
-end
--- chef/data_bags/crowbar/migrate/cinder/013_eternus_dx_volume.rb
+++
@@ -1,9 +0,0 @@
-def upgrade ta, td, a, d
-  a["volume_defaults"]["eternus"] = ta["volume_defaults"]["eternus"]
-  return a, d
-end
-
-def downgrade ta, td, a, d
-  a["volume_defaults"].delete("eternus")
-  return a, d
-end
